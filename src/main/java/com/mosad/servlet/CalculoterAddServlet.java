52b3e5a5b3580142bcdad42905d4fb4e3aa4a22a
Mosad-Sayed <55459416+Mosad-Sayed@users.noreply.github.com>
2022-05-13 16:30:11 +0200

    Add files via upload

---
+++ src/main/java/com/mosad/servlet/CalculoterAddServlet.java
@@ -0,0 +1,28 @@
+package com.mosad.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "calculoterAdd", urlPatterns = { "/calculoter/add" })
+public class CalculoterAddServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		try {
+			String n1 = request.getParameter("n1");
+			String n2 = request.getParameter("n2");
+			int number1 = Integer.parseInt(n1);
+			int number2 = Integer.parseInt(n2);
+			response.getWriter().println(number1 + number2);
+		} catch (Exception e) {
+			response.getWriter().println("please pass valid values");
+		}
+	}
+
+}
